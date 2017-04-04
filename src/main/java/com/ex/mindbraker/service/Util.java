af8fe98768aaa768d38c4cf3eb7af2a5e28fa05f
BohdanLaba <bohdan.laba19991@gmail.com>
2017-04-05 01:18:03 +0300

    added rotation util

--- src/main/java/com/ex/mindbraker/service/Util.java
+++ src/main/java/com/ex/mindbraker/service/Util.java
@@ -7,6 +7,7 @@ import java.util.Random;
 import com.ex.mindbraker.models.Circle;
 import com.ex.mindbraker.models.Triangle;
 import com.ex.mindbraker.models.Vertex;
+import com.ex.mindbraker.models.VertexNames;
 
 public class Util {
 	private static String VERTEX = "Vertex";
@@ -33,12 +34,55 @@ public class Util {
 
 		return true;
 	}
-	
-	public static  Deque<Vertex> rotateVertexes(Deque<Vertex> vertexes) {
+
+	public static Deque<Vertex> rotateVertexes(Deque<Vertex> vertexes) {
 		Vertex lastVertex = vertexes.pollLast();
 		vertexes.push(lastVertex);
 		return vertexes;
 	}
-	
-	public static
+
+	public static void rotateVertexesClockwise(Vertex[][] vertexes) {
+		if (!vertexes[0][1].getName().isEmpty() && !vertexes[0][1].getName().equals(VertexNames.NULL.toString())) {
+			vertexes[0][2].setName(vertexes[0][1].getName());
+			vertexes[0][1].setName(VertexNames.NULL.toString());
+
+			vertexes[2][1].setName(vertexes[2][2].getName());
+			vertexes[2][2].setName(VertexNames.NULL.toString());
+
+			vertexes[0][0].setName(vertexes[2][0].getName());
+			vertexes[2][0].setName(VertexNames.NULL.toString());
+		} else {
+			vertexes[2][2].setName(vertexes[0][2].getName());
+			vertexes[0][2].setName(VertexNames.NULL.toString());
+
+			vertexes[2][0].setName(vertexes[2][1].getName());
+			vertexes[2][1].setName(VertexNames.NULL.toString());
+
+			vertexes[0][1].setName(vertexes[0][0].getName());
+			vertexes[0][0].setName(VertexNames.NULL.toString());
+		}
+	}
+
+	public static void rotateVertexesCounterClockwise(Vertex[][] vertexes) {
+		if (!vertexes[0][1].getName().isEmpty() && !vertexes[0][1].getName().equals(VertexNames.NULL.toString())) {
+			vertexes[0][0].setName(vertexes[0][1].getName());
+			vertexes[0][1].setName(VertexNames.NULL.toString());
+
+			vertexes[0][2].setName(vertexes[2][2].getName());
+			vertexes[2][2].setName(VertexNames.NULL.toString());
+
+			vertexes[2][1].setName(vertexes[2][0].getName());
+			vertexes[2][0].setName(VertexNames.NULL.toString());
+		} else {
+			vertexes[0][1].setName(vertexes[0][2].getName());
+			vertexes[0][2].setName(VertexNames.NULL.toString());
+
+			vertexes[2][2].setName(vertexes[2][1].getName());
+			vertexes[2][1].setName(VertexNames.NULL.toString());
+
+			vertexes[2][0].setName(vertexes[0][0].getName());
+			vertexes[0][0].setName(VertexNames.NULL.toString());
+		}
+
+	}
 }
